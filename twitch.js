6d4739c23bad688163ae2e9fc9e90403596cfbb9
SandyWyper <sandywyper@gmail.com>
2018-01-23 17:32:23 +0000

    it worksgit statusgit status Mohahahahah!

--- twitch.js
+++ twitch.js
@@ -1,61 +1,33 @@
-
 const userName = ['ekolimits', 'sinatraa', 'dewchallenge', 'sir_thomas_', 'wildlands', 'bigfoltz', 'aceyummy', 'opex', 'xiceman', 'freecodecamp'];
 let userData = [];
 let usersOnline = [];
+let counter = 0;
 
 
 $(document).ready(function() {
 
   $("#menuIcon").on("click", menuBar);
-  $("#Go").on("click", go);
   $("#allUsers").on("click", allUsersTab);
   $("#onLine").on("click", onLineTab);
   $("#offLine").on("click", offLineTab);
   $(".searchButton").on("click", searchUserList);
 
   runPage();
+
 });
 
 // "runPage" initiates the ajax calls and fills the arrays with all the data
-//   needed for the page - however i'm not sure how to run "displayUsers" & "usersOnLine"
-//  only when the arrays are filled or all ajax requests have come back, success or fail.
+//   needed for the page
+
 
 function runPage() {
-
-  // $.when(fetch(), getUsersLive()).done(displayUsers, usersOnLine);
-
-
-fetchUserData();
-getUsersLive();
-
-//
-// Trying something like....
-// $.when(fetch(),getUsersLive()).done(displaystuff, onLine);
+  fetchUserData();
+  getUsersLive();
 }
-
-
-//identifies thelink inbedded in a div and opens it in a new windo on click
-function linkOut() {
-  let linkToTwitch = $(this).find("a").attr("href");
-  window.open(linkToTwitch);
-  return false;
-}
-
-//responsive menubar dependent on screen width.
-function menuBar() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className = "responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-
 
 //username.length number of calls to api - populating favourite user details
 function fetchUserData() {
-  for (x = 0; x < userName.length ; x++) {
+  for (x = 0; x < userName.length; x++) {
     $.ajax({
       type: 'GET',
       url: 'https://api.twitch.tv/kraken/channels/' + userName[x],
@@ -64,15 +36,18 @@ function fetchUserData() {
       },
       success: function(data) {
         userData[data.name] = data;
+        counter++;
+        if (counter === userName.length * 2) {
+          displayUsers();
+        }
       }
     });
   }
 }
 
-
 //username.length number of calls to api - "streams" to check on-line users
 function getUsersLive() {
-  for (i = 0; i < userName.length ; i++) {
+  for (i = 0; i < userName.length; i++) {
     $.ajax({
       type: 'GET',
       url: 'https://api.twitch.tv/kraken/streams/' + userName[i],
@@ -80,42 +55,61 @@ function getUsersLive() {
         'Client-ID': '3jvb8k1jin691y6ti00uea9bfxwruz'
       },
       success: function(data) {
+        counter++;
         if (data.stream) {
           usersOnline[data.stream.channel.name] = data;
+        }
+        if (counter === userName.length * 2) {
+          displayUsers();
         }
       }
     });
   }
 }
 
-
 function displayUsers() {
- for ( j=0 ; j < userName.length ; j++) {
+  for (j = 0; j < userName.length; j++) {
     $('.UserList').append($('<div class="row UserBox OffLineClass" id="' + userData[userName[j]].name + '">' +
-    '<div class="col">' +
-    '<img src="' + userData[userName[j]].logo +'"/>' +
-    '</div>' +
-    '<div class="col-9 userName">' +
-    '<span>' + userData[userName[j]].display_name + '</span>' +
-    '<em class="userInfo">' + '<p>' + userData[userName[j]].game + '</p>' + '</em>' +
-    '<em class="userInfo">' + '<span>' +"<a href='" +  userData[userName[j]].url +"'>" + '</span>' + '</em>' +
-    '</div>' + '</div>'));
-}
-$(".UserBox").on("click", linkOut);
+      '<div class="col">' +
+      '<img src="' + userData[userName[j]].logo + '"/>' +
+      '</div>' +
+      '<div class="col-9 userName">' +
+      '<span>' + userData[userName[j]].display_name + '</span>' +
+      '<em class="userInfo">' + '<p>' + userData[userName[j]].game + '</p>' + '</em>' +
+      '<em class="userInfo">' + '<span>' + "<a href='" + userData[userName[j]].url + "'>" + '</span>' + '</em>' +
+      '</div>' + '</div>'));
+  }
+  $(".UserBox").on("click", linkOut);
 
-usersOnLine();
+  usersOnLine();
 }
-
 
 //Adds "OnLineClass" to users that are currently streaming
 function usersOnLine() {
-for ( z=0 ; z<userName.length ; z++ ) {
-  if (usersOnline[userName[z]]) {
-    let live = document.getElementById(userName[z]);
-    live.classList.add("OnLineClass");
-    live.classList.remove("OffLineClass");
+  for (z = 0; z < userName.length; z++) {
+    if (usersOnline[userName[z]]) {
+      const live = document.getElementById(userName[z]);
+      live.classList.add("OnLineClass");
+      live.classList.remove("OffLineClass");
+    }
   }
 }
+
+//responsive menubar dependent on screen width.
+function menuBar() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className = "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//identifies thelink inbedded in a div and opens it in a new windo on click
+function linkOut() {
+  let linkToTwitch = $(this).find("a").attr("href");
+  window.open(linkToTwitch);
+  return false;
 }
 
 function allUsersTab() {
@@ -146,14 +140,14 @@ function searchUserList() {
   let searchInput = takeSearchString(event);
   let index = findIndex(searchInput);
 
-if (index >= 0) {
-  $('.OnLineClass').hide();
-  $('.OffLineClass').hide();
-  const userLook = "#" + userName[index];
-  $(userLook).show();
-} else {
-  alert("user not found");
-}
+  if (index >= 0) {
+    $('.OnLineClass').hide();
+    $('.OffLineClass').hide();
+    const userLook = "#" + userName[index];
+    $(userLook).show();
+  } else {
+    alert("user not found");
+  }
 }
 
 function takeSearchString(event) {
@@ -162,14 +156,6 @@ function takeSearchString(event) {
   return userSearch.toLowerCase();
 }
 
-function findIndex(searchString){
-return userName.indexOf(searchString);
-}
-// initiates second phase
-
-function go() {
-
-
-displayUsers();
-
+function findIndex(searchString) {
+  return userName.indexOf(searchString);
 }
